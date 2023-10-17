2a7103d398d5fec1ff6d2fef9ce04a96b53d4ff2
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-10-17 00:20:37 -0400

    VPC Subnets routetables and associateion,gateways and nat gateways through modules concept and automated terraform with Docdb and load balancer+ Aurora DB + RDS + Elasticache + RabbitMQ

--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,3 @@ yum install ansible -y &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/maheshkoheda/roboshop-ansible.git main.yml -e component= rabbitmq &>>/opt/userdata.log
 
 
-
