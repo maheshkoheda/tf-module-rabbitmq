548d7bee7a0e5b4eb3d556a153995c70e4f243e8
Koheda Mahesh <maheshkoheda5@gmail.com>
2023-10-16 23:56:55 -0400

    VPC Subnets routetables and associateion,gateways and nat gateways through modules concept and automated terraform with Docdb and load balancer+ Aurora DB + RDS + Elasticache + RabbitMQ

--- main.tf
+++ main.tf
@@ -38,5 +38,6 @@ resource "aws_instance" "main" {
   vpc_security_group_ids = [aws_security_group.main.id]
   subnet_id = var.subnet_ids[0]
   tags = merge(local.tags, {Name = "${local.name_prefix}-rabbitmq" })
+  user_data = "${path.module}/userdata.sh"
 
 }
